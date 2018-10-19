ebb6e56a55ce520b28beca880fa17f50b474333f
Lukasz Klis <lukasz@klis.pl>
2018-10-19 13:44:48 +0200

    :bug: Add missing global announcements

--- src/command/show-updates.ts
+++ src/command/show-updates.ts
@@ -3,15 +3,27 @@ import * as figures from "figures";
 import { CrawlerDataDto } from "../model/dto/crawler-data-dto";
 import { eduxUrl } from "../config/edux";
 import { mapCourses } from "../helper/course-helper";
+import { storeValue } from "../helper/store-helper";
+import { announcementsCountKey } from "../config/store";
+import { hasNewAnnouncements } from "../helper/announcement-helper";
 
 export function showUpdates(crawlerData: CrawlerDataDto): void {
-    const { courseElements, courseSections } = crawlerData;
+    const {
+        announcementRowsCount,
+        courseElements,
+        courseSections
+    } = crawlerData;
     const updatedCourses = mapCourses(courseElements, courseSections).filter(
         course => course.updatedSections.length > 0
     );
 
-    if (updatedCourses.length === 0) {
-        console.log(chalk.green(`\n   No new updates! ${chalk.red(figures.heart)}`));
+    if (
+        updatedCourses.length === 0 &&
+        !hasNewAnnouncements(announcementRowsCount)
+    ) {
+        console.log(
+            chalk.green(`\n   No new updates! ${chalk.red(figures.heart)}`)
+        );
         return;
     } else {
         console.log(`\n   ${chalk.yellow("New updates found:\n")}`);
@@ -24,4 +36,17 @@ export function showUpdates(crawlerData: CrawlerDataDto): void {
             )}:\n     ${chalk.blue.underline(`${eduxUrl}/${course.link}`)}\n`
         );
     });
+
+    if (!hasNewAnnouncements(announcementRowsCount)) {
+        return;
+    }
+
+    storeValue<number>(announcementsCountKey, announcementRowsCount);
+    console.log(
+        `   ${chalk.yellow(figures.arrowRight)} There are ${chalk.cyan(
+            `${announcementRowsCount}`
+        )} new announcements. Read them by clicking on the "Announcements" tab:\n     ${chalk.blue.underline(
+            `${eduxUrl}`
+        )}\n`
+    );
 }
